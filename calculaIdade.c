9b6c596036347d3cfb4a12c6d8d2b53499981076
Guilherme-RSM <m.guilherme3@gmail.com>
2023-10-04 17:13:31 -0300

    Algoritimo que calcula a idade

--- calculaIdade.c
+++ calculaIdade.c
@@ -1,1 +1,22 @@
 #include <stdio.h>
+int main(){
+    int anoNascimento, anoAtual, idade;
+    anoNascimento = 0;
+    anoAtual = 0;
+    idade = 0;
+
+    printf ("Digite o ano em que voce nasceu: \n");
+    scanf("%d", &anoNascimento);
+
+    printf("Digite o ano atual: \n");
+    scanf("%d", &anoAtual);
+    
+    idade = anoAtual - anoNascimento;
+
+    printf("Sua idade e  %d : \n", idade);
+
+
+    system("pause");
+
+
+}
